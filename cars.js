c6e2c7163330b6fecd5c4ca62ea3a808a7fbbcc1
Yuna <ilena@f4d3z.space>
2022-03-16 20:51:53 +0000

    getting ready for mini slideshow functionality

--- cars.js
+++ cars.js
@@ -3,18 +3,27 @@ var products = {
     name : "Car 1",
     desc : "first car listing",
     img : "bmw.jpg",
+    img2 : "bmw.jpg",
+    img3 : "bmw.jpg",
+    img4 : "bmw.jpg",
     price : 8000
   },
   2: {
     name : "Car 2",
     desc : "second car listing",
     img : "bmw.jpg",
+    img2 : "bmw.jpg",
+    img3 : "bmw.jpg",
+    img4 : "bmw.jpg",
     price : 4000
   },
   3: {
     name : "Car 3",
     desc : "third car listing",
     img : "bmw.jpg",
+    img2 : "bmw.jpg",
+    img3 : "bmw.jpg",
+    img4 : "bmw.jpg",
     price : 9000
   },
 };
