92cff3accbcac430cf2599665b749ff30fbf5972
Ollydeldie <98750663+Ollydeldie@users.noreply.github.com>
2022-03-21 17:41:02 +0000

    Update cars.js

--- cars.js
+++ cars.js
@@ -2,7 +2,7 @@ var products = {
   1: {
     name : "BMW",
     desc : "first car listing",
-    img : "bmw.jpg", width="200" height="190"
+    img : "bmw.jpg", 
     img2 : "bmw.jpg",
     img3 : "bmw.jpg",
     img4 : "bmw.jpg",
