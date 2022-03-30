2c311a0d169cee19afd6dfc4ad62116477ed3c15
Ollydeldie <98750663+Ollydeldie@users.noreply.github.com>
2022-03-30 14:47:11 +0100

    Update cars.js

--- cars.js
+++ cars.js
@@ -35,4 +35,29 @@ var products = {
     img4 : "bmw.jpg",
     price : 11000
   },
+  <div class="slideshow-container">
+
+<div class="mySlides fade">
+  <div class="numbertext">1 / 3</div>
+  <img src="bmw.jpg" style="width:100%">
+  <div class="text">Caption Text</div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">2 / 3</div>
+  <img src="bmw.jpg" style="width:100%">
+  <div class="text">Caption Two</div>
+</div>
+
+<div class="mySlides fade">
+  <div class="numbertext">3 / 3</div>
+  <img src="bmw.jpg" style="width:100%">
+  <div class="text">Caption Three</div>
+</div>
+
+<a class="prev" onclick="plusSlides(-1)">❮</a>
+<a class="next" onclick="plusSlides(1)">❯</a>
+
+</div>
+<br>
 };
